195af37ed8cd05b22883c74956c3f56e03fc62f2
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-06-22 19:22:59 +0900

    README 추가

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// database 연결
 db.connect();
 // db.end();
 
