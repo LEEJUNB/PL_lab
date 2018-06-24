4e50dd6d810b03c4ea1fdc569987e20cb39fda8f
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-06-24 20:20:39 +0900

    model 과  라우터들 추가

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const user = require('./user');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.use('/user',user);
 
 module.exports = router;
