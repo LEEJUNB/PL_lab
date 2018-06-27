56ed2e061b22ce61e6c7341e94ed88e5abd3af3c
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-06-27 23:30:23 +0900

    내용 없는 model 삭제

--- db/index.js
+++ db/index.js
@@ -1,28 +1,34 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const config = require('../setting.json').database;
+const config = require("../setting.json").database;
 
-const sequelize = new Sequelize(config.databaseSchema, config.user, config.password, {
-  host: config.host,
-  dialect: config.database
-});
+const sequelize = new Sequelize(
+  config.databaseSchema,
+  config.user,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.database
+  }
+);
 
 const __modelPath = __dirname + `/model`;
 const db = {};
 fs.readdirSync(__modelPath)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== 'index.js' && file !== 'config.js');
+    return (
+      file.indexOf(".") !== 0 && (file !== "index.js" && file !== "config.js")
+    );
   })
   .forEach(function(file) {
-    const model = sequelize['import'](path.join(__modelPath, file));
+    const model = sequelize["import"](path.join(__modelPath, file));
     model.sync();
     db[model.name] = model;
   });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 
 module.exports = db;
